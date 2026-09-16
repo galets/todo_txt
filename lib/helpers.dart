ea84caab04be9a1bf07edef9176d568e3916deab
galets <git5511@mx1.galets.net>
2026-09-16 10:20:59 -0400

    use functions instead of lambdas

--- lib/helpers.dart
+++ lib/helpers.dart
@@ -3,17 +3,21 @@ import 'dart:io';
 RegExp dateRegex =
     RegExp(r'(\d{4})[\-](0?[1-9]|1[012])[\-](0?[1-9]|[12][0-9]|3[01])$');
 
-bool isPriorityString(String prioString) =>
-    prioString.length == 3 &&
-    prioString[0] == '(' &&
-    prioString[2] == ')' &&
-    prioString.codeUnitAt(1) >= 65 &&
-    prioString.codeUnitAt(1) <= 90;
+bool isPriorityString(String prioString) {
+  return prioString.length == 3 &&
+      prioString[0] == '(' &&
+      prioString[2] == ')' &&
+      prioString.codeUnitAt(1) >= 65 &&
+      prioString.codeUnitAt(1) <= 90;
+}
 
-bool isDateString(String date) => dateRegex.hasMatch(date);
+bool isDateString(String date) {
+  return dateRegex.hasMatch(date);
+}
 
-String dateToDateString(DateTime date) =>
-    '${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+String dateToDateString(DateTime date) {
+  return '${date.year.toString().padLeft(4, '0')}-${date.month.toString().padLeft(2, '0')}-${date.day.toString().padLeft(2, '0')}';
+}
 
 String paramsToString(Map params) {
   var strParams = '';
@@ -21,5 +25,6 @@ String paramsToString(Map params) {
   return strParams.trim();
 }
 
-String pathToPlatformPath(String path) =>
-    path.replaceAll(RegExp(r'\\|\/'), Platform.pathSeparator);
+String pathToPlatformPath(String path) {
+  return path.replaceAll(RegExp(r'\\|\/'), Platform.pathSeparator);
+}
